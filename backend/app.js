52f97dc0dab423bf20b925377a36037193c682f4
kedar6160 <kedardixit6160@gmail.com>
2024-03-08 18:21:57 +0530

    authController added packages as well

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const authRoutes = require("./routes/authRoutes");
 const recipeRoutes = require("./routes/recipeRoutes");
 const ingredientRoutes = require("./routes/ingredientRoutes");
 const reportRoutes = require("./routes/reportRoutes");
-
+    
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=> console.log(`server started on port ${PORT}`));
--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,11 +1,35 @@
-exports.login = (req, res) => {
+const bcrypt =require("bcryptjs");
+const jwt =require("jsonwebtoken");
+const config =require("../config/config");
+const User =require("../models/User");
+
+  exports.register = async (req, res) => {
+    const {username, password, role} =req.body;
+    try{
+      let user = await User.findOne({username});
+      if (user){
+        return res.status(400).json({message: "User already exists"});
+      }
+      user = new User({
+        username,
+        password,
+        role
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password,salt);
+
+      await user.save();
+      res.status(201).json({message: "User registered successfully"});
+    }
+    catch(err){
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  };
+  exports.login = (req, res) => {
     // Implementation for login endpoint
   };
-  
-  exports.register = (req, res) => {
-    // Implementation for register endpoint
-  };
-  
   exports.logout = (req, res) => {
     // Implementation for logout endpoint
   };
